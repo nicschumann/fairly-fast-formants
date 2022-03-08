53a01f60319681a92d345db139c17fc3a263a609
nicschumann <nic@nicschumann.co>
2022-03-08 09:39:25 -0500

    [build] changes production build process to ignore assets

--- vite.build.config.js
+++ vite.build.config.js
@@ -3,10 +3,11 @@ import { defineConfig } from 'vite';
 import wasmPack from 'vite-plugin-wasm-pack';
 
 export default defineConfig({
+  publicDir: "./fake-public",
   build: {
     lib: {
       entry: path.resolve(__dirname, 'formants-js/index.ts'),
-      name: 'formants',
+      name: 'FairlyFastFormants',
       fileName: (format) => `formants.${format}.js`
     },
     minify: false
