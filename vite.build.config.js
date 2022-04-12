13a4ddd9757a31655947c41cd5ca796d954ecaa1
nicschumann <nic@nicschumann.co>
2022-04-12 11:13:48 -0700

    [build] ignores mathjs dependency (used for kalman filter prototyping)

--- vite.build.config.js
+++ vite.build.config.js
@@ -5,6 +5,13 @@ import wasmPack from 'vite-plugin-wasm-pack';
 export default defineConfig({
   publicDir: "./fake-public",
   build: {
+    rollupOptions: {
+      external: [
+        // ignore mathjs dependencies; these are only used
+        // for prototyping, and are not used in production.
+        'mathjs'
+      ]
+    },
     lib: {
       entry: path.resolve(__dirname, 'formants-js/index.ts'),
       name: 'FairlyFastFormants',
